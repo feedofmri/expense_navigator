6919d7cf54484378cc9ac0c8f00bbc65f56ff2bc
Md. Rubayet Islam <77844439+feedofmri@users.noreply.github.com>
2025-03-19 12:09:00 +0600

    Initial commit

---
+++ android/app/src/main/java/io/boraq/expense_navigator/MainActivity.java
@@ -0,0 +1,6 @@
+package io.boraq.expense_navigator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/provider/transaction.provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class TransactionProvider with ChangeNotifier {
+
+  // For calculating the total amount of income
+
+}
